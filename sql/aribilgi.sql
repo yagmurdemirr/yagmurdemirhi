9d6728396ac7eafdf916b443e23baebd7cc2b44b
yagmurdemirr <yagmur@genpower.gen.tr>
2022-04-06 10:34:51 +0300

    Veri Tabanı Güncellendi

--- sql/aribilgi.sql
+++ sql/aribilgi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Anamakine: 127.0.0.1
--- Üretim Zamanı: 29 Mar 2022, 14:36:01
+-- Üretim Zamanı: 06 Nis 2022, 09:34:04
 -- Sunucu sürümü: 10.4.22-MariaDB
 -- PHP Sürümü: 8.0.15
 
@@ -73,6 +73,36 @@ INSERT INTO `ornek` (`id`, `ad`, `yas`, `il`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Tablo için tablo yapısı `sayfalar`
+--
+
+CREATE TABLE `sayfalar` (
+  `id` int(11) NOT NULL,
+  `baslik` varchar(100) NOT NULL,
+  `icerik` text NOT NULL,
+  `meta` varchar(160) NOT NULL,
+  `foto` varchar(50) NOT NULL,
+  `fotoalt` varchar(90) NOT NULL,
+  `seotitle` varchar(70) NOT NULL,
+  `durum` varchar(10) NOT NULL,
+  `sayfaturu` varchar(10) NOT NULL,
+  `tarih` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Tablo döküm verisi `sayfalar`
+--
+
+INSERT INTO `sayfalar` (`id`, `baslik`, `icerik`, `meta`, `foto`, `fotoalt`, `seotitle`, `durum`, `sayfaturu`, `tarih`) VALUES
+(1, 'Hakkımda', '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi dolor temporibus qui provident reiciendis quidem, culpa nostrum, fugit animi quasi, corporis iure sed maiores earum minus tenetur iste accusamus modi consectetur expedita ducimus blanditiis placeat. Voluptatem eaque animi perferendis, sequi ex natus vel iusto adipisci autem aliquam commodi? Quasi, totam!</p>\r\n\r\n<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi dolor temporibus qui provident reiciendis quidem, culpa nostrum, fugit animi quasi, corporis iure sed maiores earum minus tenetur iste accusamus modi consectetur expedita ducimus blanditiis placeat. Voluptatem eaque animi perferendis, sequi ex natus vel iusto adipisci autem aliquam commodi? Quasi, totam!</p>\r\n\r\n<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi dolor temporibus qui provident reiciendis quidem, culpa nostrum, fugit animi quasi, corporis iure sed maiores earum minus tenetur iste accusamus modi consectetur expedita ducimus blanditiis placeat. Voluptatem eaque animi perferendis, sequi ex natus vel iusto adipisci autem aliquam commodi? Quasi, totam!</p>\r\n\r\n<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi dolor temporibus qui provident reiciendis quidem, culpa nostrum, fugit animi quasi, corporis iure sed maiores earum minus tenetur iste accusamus modi consectetur expedita ducimus blanditiis placeat. Voluptatem eaque animi perferendis, sequi ex natus vel iusto adipisci autem aliquam commodi? Quasi, totam!</p>\r\n\r\n<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi dolor temporibus qui provident reiciendis quidem, culpa nostrum, fugit animi quasi, corporis iure sed maiores earum minus tenetur iste accusamus modi consectetur expedita ducimus blanditiis placeat. Voluptatem eaque animi perferendis, sequi ex natus vel iusto adipisci autem aliquam commodi? Quasi, totam!</p>\r\n', 'Lorem ipsum dolor sit amet consectetur', '../img/foto (1).jpg', 'Hakkımda', 'Hakkımda | Aribilişim', 'yayınlandı', 'üstsayfa', '2022-04-01'),
+(2, 'Web Tasarım Hizmeti', '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quas eius odio adipisci. Nemo veniam, totam expedita cupiditate modi labore placeat consectetur est assumenda alias nam magnam voluptatum excepturi id, cumque quia exercitationem? Perferendis totam commodi quae dolorum corporis autem magnam tempora blanditiis officiis quod. Eveniet laboriosam ipsa assumenda mollitia.</p>\r\n\r\n<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quas eius odio adipisci. Nemo veniam, totam expedita cupiditate modi labore placeat consectetur est assumenda alias nam magnam voluptatum excepturi id, cumque quia exercitationem? Perferendis totam commodi quae dolorum corporis autem magnam tempora blanditiis officiis quod. Eveniet laboriosam ipsa assumenda mollitia.</p>\r\n\r\n<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quas eius odio adipisci. Nemo veniam, totam expedita cupiditate modi labore placeat consectetur est assumenda alias nam magnam voluptatum excepturi id, cumque quia exercitationem? Perferendis totam commodi quae dolorum corporis autem magnam tempora blanditiis officiis quod. Eveniet laboriosam ipsa assumenda mollitia.</p>\r\n\r\n<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quas eius odio adipisci. Nemo veniam, totam expedita cupiditate modi labore placeat consectetur est assumenda alias nam magnam voluptatum excepturi id, cumque quia exercitationem? Perferendis totam commodi quae dolorum corporis autem magnam tempora blanditiis officiis quod. Eveniet laboriosam ipsa assumenda mollitia.</p>\r\n', 'Lorem ipsum dolor sit amet consectetur', '../img/foto (2).jpg', 'Web Tasarım Hizmeti', 'Web Tasarım Hizmeti | Aribilişim', 'Yayınlandı', 'Alt Sayfa', '2022-04-01'),
+(3, 'Grafik Tasarım Hizmeti', '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quas eius odio adipisci. Nemo veniam, totam expedita cupiditate modi labore placeat consectetur est assumenda alias nam magnam voluptatum excepturi id, cumque quia exercitationem? Perferendis totam commodi quae dolorum corporis autem magnam tempora blanditiis officiis quod. Eveniet laboriosam ipsa assumenda mollitia.</p>\r\n\r\n<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quas eius odio adipisci. Nemo veniam, totam expedita cupiditate modi labore placeat consectetur est assumenda alias nam magnam voluptatum excepturi id, cumque quia exercitationem? Perferendis totam commodi quae dolorum corporis autem magnam tempora blanditiis officiis quod. Eveniet laboriosam ipsa assumenda mollitia.</p>\r\n\r\n<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quas eius odio adipisci. Nemo veniam, totam expedita cupiditate modi labore placeat consectetur est assumenda alias nam magnam voluptatum excepturi id, cumque quia exercitationem? Perferendis totam commodi quae dolorum corporis autem magnam tempora blanditiis officiis quod. Eveniet laboriosam ipsa assumenda mollitia.</p>\r\n\r\n<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quas eius odio adipisci. Nemo veniam, totam expedita cupiditate modi labore placeat consectetur est assumenda alias nam magnam voluptatum excepturi id, cumque quia exercitationem? Perferendis totam commodi quae dolorum corporis autem magnam tempora blanditiis officiis quod. Eveniet laboriosam ipsa assumenda mollitia.</p>\r\n\r\n<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quas eius odio adipisci. Nemo veniam, totam expedita cupiditate modi labore placeat consectetur est assumenda alias nam magnam voluptatum excepturi id, cumque quia exercitationem? Perferendis totam commodi quae dolorum corporis autem magnam tempora blanditiis officiis quod. Eveniet laboriosam ipsa assumenda mollitia.</p>\r\n', 'Lorem ipsum dolor sit amet consectetur', '../img/foto (3).jpg', 'Grafik Tasarım Hizmeti', 'Grafik Tasarım Hizmeti | Aribilişim', 'Yayınlandı', 'Alt Sayfa', '2022-04-01'),
+(4, 'Dijital Pazarlama', '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quas eius odio adipisci. Nemo veniam, totam expedita cupiditate modi labore placeat consectetur est assumenda alias nam magnam voluptatum excepturi id, cumque quia exercitationem? Perferendis totam commodi quae dolorum corporis autem magnam tempora blanditiis officiis quod. Eveniet laboriosam ipsa assumenda mollitia.</p>\r\n\r\n<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quas eius odio adipisci. Nemo veniam, totam expedita cupiditate modi labore placeat consectetur est assumenda alias nam magnam voluptatum excepturi id, cumque quia exercitationem? Perferendis totam commodi quae dolorum corporis autem magnam tempora blanditiis officiis quod. Eveniet laboriosam ipsa assumenda mollitia.</p>\r\n\r\n<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quas eius odio adipisci. Nemo veniam, totam expedita cupiditate modi labore placeat consectetur est assumenda alias nam magnam voluptatum excepturi id, cumque quia exercitationem? Perferendis totam commodi quae dolorum corporis autem magnam tempora blanditiis officiis quod. Eveniet laboriosam ipsa assumenda mollitia.</p>\r\n\r\n<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quas eius odio adipisci. Nemo veniam, totam expedita cupiditate modi labore placeat consectetur est assumenda alias nam magnam voluptatum excepturi id, cumque quia exercitationem? Perferendis totam commodi quae dolorum corporis autem magnam tempora blanditiis officiis quod. Eveniet laboriosam ipsa assumenda mollitia.</p>\r\n\r\n<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quas eius odio adipisci. Nemo veniam, totam expedita cupiditate modi labore placeat consectetur est assumenda alias nam magnam voluptatum excepturi id, cumque quia exercitationem? Perferendis totam commodi quae dolorum corporis autem magnam tempora blanditiis officiis quod. Eveniet laboriosam ipsa assumenda mollitia.</p>\r\n', 'Lorem ipsum dolor sit amet consectetur', '../img/foto (4).jpg', 'Dijital Pazarlama', 'Dijital Pazarlama | Aribilişim', 'Yayınlandı', 'Alt Sayfa', '2022-04-01'),
+(7, 'dfhgsdhthtsjrt', '<p>sthsthrjrjrsj</p>\r\n', 'rthrfjsjsr', '../img/3500w.jpg', 'atrshtrzshzr', 'ftrhtgjg', 'Yayınlandı', 'Üst Sayfa', '2022-04-05');
+
+-- --------------------------------------------------------
+
+--
 -- Tablo için tablo yapısı `yazilar`
 --
 
@@ -93,7 +123,7 @@ CREATE TABLE `yazilar` (
 --
 
 INSERT INTO `yazilar` (`id`, `baslik`, `icerik`, `meta`, `foto`, `fotoalt`, `kategori`, `tarih`, `durum`) VALUES
-(0, 'Lorem Ipsum Dolar Sıt', ' Lorem ipsum dolor sit amet consectetur adipisicing elitLorem ipsum dolor sit amet consectetur adipisicing elitLorem ipsum dolor sit amet consectetur adipisicing elitLorem ipsum dolor sit amet consectetur adipisicing elitLorem ipsum dolor sit amet consectetur adipisicing elitLorem ipsum dolor sit amet consectetur adipisicing elit', 'Lorem ipsum dolor sit amet consectetur adipisicing elit', '../img/dijital-pazarlama-hizmeti-330x150px.jpg', 'wwwwww', 'Dijital Pazarlama', '2022-03-22', 'Yayınlandı');
+(0, 'Seo\'nun Web Siteleri İçin Önemi', '<p>dddddddddddddddddd</p>\r\n', 'dddddddddddddddddd', '../img/foto (1).jpg', 'Seo\'nun Önemi', 'Dijital Pazarlama', '2022-03-31', 'Yayınlandı');
 
 --
 -- Dökümü yapılmış tablolar için indeksler
@@ -112,6 +142,12 @@ ALTER TABLE `ornek`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Tablo için indeksler `sayfalar`
+--
+ALTER TABLE `sayfalar`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
 --
 
@@ -126,6 +162,12 @@ ALTER TABLE `kategoriler`
 --
 ALTER TABLE `ornek`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `sayfalar`
+--
+ALTER TABLE `sayfalar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
