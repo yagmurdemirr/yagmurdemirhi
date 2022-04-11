8d3246c3f3b733a59e7aad42bfa7d1f8b86d10b3
yagmurdemirr <yagmur@genpower.gen.tr>
2022-04-11 12:45:32 +0300

    İletişim Sayfası Oluşturuldu

--- iletisim.php
+++ iletisim.php
@@ -1,16 +1,16 @@
-<?php 
+<?php
 
-require_once('header.php'); 
+require_once('header.php');
 
-$sorgu_iletisim = $db -> prepare('select *form ayarlar order by id desc limit 1');
-$sorgu_iletisim -> execute();
-$satir_iletisim = $sorgu_iletisim -> fetch();
+$sorgu_iletisim = $db->prepare('select *from ayarlar order by id desc limit 1');
+$sorgu_iletisim->execute();
+$satir_iletisim = $sorgu_iletisim->fetch();
 
 ?>
 
 <!-- İletisim Banner Section Start -->
 
-<section id="iletisimBanner" class="py-5" style="background-image: <?php echo $satir_iletisim['iletisimbanner']; ?> ;">
+<section id="iletisimBanner" class="py-15" style="background-image:url('<?php echo substr($satir_iletisim['iletisimbanner'], 3); ?>') ; background-size:cover; background-repeat:no-repeat;">
     <div class="container">
         <div class="row">
             <div class="col-12 text-center">
@@ -22,6 +22,76 @@ $satir_iletisim = $sorgu_iletisim -> fetch();
 </section>
 
 <!-- İletisim Banner Section End -->
+
+<!-- İnfo Section Start -->
+
+<section id="info" class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 text-center">
+            <i class="bi bi-geo mor"></i> <br>
+            <strong>Adres</strong> <br>
+            <?php echo $satir_iletisim['adres']; ?>
+            </div>
+            <div class="col-md-4 text-center">
+            <i class="bi bi-telephone mor"></i> <br>
+            <strong>Telefon</strong> <br>
+            <a class="text-decoration-none text-dark" href="tel:<?php echo $satir_iletisim['telefon']; ?>"><?php echo $satir_iletisim['telefon']; ?></a>
+            </div>
+            <div class="col-md-4 text-center">
+            <i class="bi bi-envelope mor"></i> <br>
+            <strong>E-Posta</strong> <br>
+            <a class="text-decoration-none text-dark" href="mailto: <?php echo $satir_iletisim['email']; ?>"><?php echo $satir_iletisim['email']; ?></a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- İnfo Section End -->
+
+<!-- Form Section Start -->
+
+<section id="iletisimForm" class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Formu Doldurun</h2>
+                <form method="post">
+                    <div class="form-group">
+                        <label><small>Adınız Soyadınız</small></label>
+                        <input type="text" name="ad" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label><small>E-Posta Adresiniz</small></label>
+                        <input type="email" name="email" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label><small>Konu</small></label>
+                        <select name="konu" class="form-control">
+                            <option value="">Seçiniz</option>
+                            <option value="Şikayet">Şikayet</option>
+                            <option value="Öneri">Öneri</option>
+                            <option value="Bilgi Talebi">Bilgi Talebi</option>
+                            <option value="Teknik Destek">Teknik Destek</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label><small>Mesajınız</small></label>
+                        <textarea name="mesaj" rows="5" class="form-control"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-mor w-100">Gönder</button>
+                    </div>
+                </form>
+            </div>
+            <div class="col-md-6 text-center">
+                <?php echo $satir_iletisim['harita']; ?>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Form Section End -->
 
 
 
