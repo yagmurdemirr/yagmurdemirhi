5c21a7680ba48fa08fbc1dde4cd43bafbb69f009
yagmurdemirr <yagmur@genpower.gen.tr>
2022-05-09 17:54:10 +0300

    sidebar.php sayfasına başlandı

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,3 @@
-<?php require_once('header.php'); ?>
-
 <!-- Sidebar Section Start -->
 
 <div class="col-md-3">
@@ -19,8 +17,81 @@
             </div>
         </div>
     </div>
+    <div class="row">
+        <div class="col-12">
+            <div class="card shadow">
+                <div class="card-body">
+                    <h3 class="mor">Kategoriler</h3>
+                    <?php
+
+                    $sorgu_katlist = $db->prepare('select *from kategoriler order by kategori asc');
+                    $sorgu_katlist->execute();
+
+                    if ($sorgu_katlist->rowCount()) {
+                        foreach ($sorgu_katlist as $satir_katlist) {
+                    ?>
+                            <a href="kategoripage.php?kategori=<?php echo $satir_katlist['kategori']; ?>" class="text-dark text-decoration-none"><?php echo $satir_katlist['kategori']; ?></a> <br>
+                    <?php
+                        }
+                    }
+
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-2">
+        <div class="col-12">
+            <div class="card shadow">
+                <div class="card-body">
+                    <h3 class="mor">Yazılar</h3>
+                    <?php
+                    $sorgu_yazilar = $db->prepare('select *from yazilar order by id desc limit 5');
+                    $sorgu_yazilar->execute();
+
+                    if ($sorgu_yazilar->rowCount()) {
+                        foreach ($sorgu_yazilar as $satir_yazilar) {
+                    ?>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <a href="sample.php?id=<?php echo $satir_yazilar['id']; ?>"><img src="<?php echo substr($satir_yazilar['foto'], 3); ?>" alt="<?php echo $satir_yazilar['fotoalt']; ?>" class="img-fluid"></a>
+                                </div>
+                                <div class="col-md-6 smallyazi">
+                                    <small><?php echo substr($satir_yazilar['icerik'], 0, 30); ?></small>
+                                </div>
+                            </div>
+                            <hr>
+                    <?php
+                        }
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <div class="card shadow">
+                <div class="card-body">
+                    <h3 class="mor">Bizi Takip Edin.</h3>
+                    <?php
+                    $sorgu_sosmed = $db->prepare('select *from ayarlar');
+                    $sorgu_sosmed->execute();
+                    $satir_sosmed = $sorgu_sosmed->fetch;
+                    
+                    if ($sorgu_sosmed->rowCount()) {
+                    ?>
+
+                        <a href="<?php echo $satir_sosmed['facebook']; ?>"><i class="bi bi-facebook"></i></a>
+
+
+                    <?php
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <!-- Sidebar Section End -->
-
-<?php require_once('footer.php'); ?>
